412b1ddd72ea2c94312d6617887fe4fea6aa9c2f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 13:27:10 +0530

    SignupVC refactored

--- SKATE with Heart/Scenes/Signup/SignupVC.swift
+++ SKATE with Heart/Scenes/Signup/SignupVC.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class SignupVC: UIViewController {
     
+    // MARK: Properties
     fileprivate let signupViewModel = SignUpViewModel()
 
     fileprivate let gradientLayer = CAGradientLayer()
@@ -11,7 +12,6 @@ class SignupVC: UIViewController {
     fileprivate let signupButton = SHButton(backgroundColor: UIColor.appColor(color: .lightGray), title: "Sign Up", titleColor: .gray, radius: 25, fontSize: 24)
     fileprivate let goToLoginButton = SHButton(backgroundColor: .clear, title: "Go to login", titleColor: .white, radius: 0, fontSize: 18)
 
-    
     fileprivate lazy var verticalStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [fullNameTextField, emailTextField, passwordTextField, signupButton])
         stackView.axis = .vertical
@@ -21,6 +21,7 @@ class SignupVC: UIViewController {
     }()
 
     
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGradient()
@@ -29,6 +30,7 @@ class SignupVC: UIViewController {
         setupRegistrationViewModelObserver()
         setupNotifications()
     }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -40,8 +42,12 @@ class SignupVC: UIViewController {
         super.viewWillDisappear(animated)
         NotificationCenter.default.removeObserver(self)
     }
-    
-    
+}
+
+
+// MARK: - Objc Methods
+extension SignupVC {
+     
     @objc fileprivate func handleTapDismiss() {
         view.endEditing(true)
     }
@@ -82,14 +88,17 @@ class SignupVC: UIViewController {
         let difference = keyboardFrame.height - bottomSpace
         self.view.transform = CGAffineTransform(translationX: 0, y: -(difference + 10))
     }
-    
+}
+
+// MARK: - Methods
+extension SignupVC {
     
     fileprivate func setupNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
-    
+        
+        
     fileprivate func navigateToHome() {
         let controller = SHTabBar()
         controller.modalPresentationStyle = .fullScreen
@@ -119,16 +128,17 @@ class SignupVC: UIViewController {
             }
         }
     }
-    
-    
+        
+        
     fileprivate func addTargets() {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
         fullNameTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         emailTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
         signupButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
-//        goToLoginButton.addTarget(self, action: #selector(handleGoToLogin), for: .touchUpInside)
+    //       goToLoginButton.addTarget(self, action: #selector(handleGoToLogin), for: .touchUpInside)
     }
+    
     
     fileprivate func setupGradient() {
         gradientLayer.colors = [UIColor.appColor(color: .orange).cgColor, UIColor.appColor(color: .pink).cgColor]
@@ -148,9 +158,9 @@ class SignupVC: UIViewController {
         
         view.addSubview(verticalStackView)
         verticalStackView.centerInSuperview()
-        verticalStackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        verticalStackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0,left: 50, bottom: 0, right: 50))
         
         view.addSubview(goToLoginButton)
-        goToLoginButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        goToLoginButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing:view.trailingAnchor)
     }
 }
