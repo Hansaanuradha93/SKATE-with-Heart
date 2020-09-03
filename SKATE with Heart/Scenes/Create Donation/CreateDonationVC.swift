5a6be36f927c45cacc95f4ac388ce7f077d506f4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 10:10:13 +0530

    Scenes added

---
+++ SKATE with Heart/Scenes/Create Donation/CreateDonationVC.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class CreateDonationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ SKATE with Heart/Scenes/Donations List/DonationListVC.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class DonationListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
