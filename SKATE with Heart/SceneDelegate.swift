c5cbbe478341997ebaa1e4ecbfe83ebb6591890c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 19:30:33 +0530

    Authentication logic added

--- SKATE with Heart/SceneDelegate.swift
+++ SKATE with Heart/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -6,12 +7,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-       guard let windowScene = (scene as? UIWindowScene) else { return }
-       let controller = LoginVC()
-       let window = UIWindow(windowScene: windowScene)
-       window.rootViewController = controller
-       self.window = window
-       window.makeKeyAndVisible()
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        var controller: UIViewController!
+        if checkIfUserLoggedIn() {
+            controller = SHTabBar()
+        } else {
+            controller = LoginVC()
+        }
+
+        window.rootViewController = controller
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+    
+    fileprivate func checkIfUserLoggedIn() -> Bool {
+        if let _ = Auth.auth().currentUser {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
