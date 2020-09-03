f3a1affdb61351ab9e2435021f937c8ea4276f76
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 12:29:19 +0530

    SFButton added

---
+++ SKATE with Heart/Custom Views/Buttons/SHButton.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class SHButton: UIButton {
+
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+
+    
+    convenience init(backgroundColor: UIColor = .white, title: String = "", titleColor: UIColor = .black, radius: CGFloat = 0, fontSize: CGFloat = 32) {
+        self.init(frame: .zero)
+        self.setup(backgroundColor: backgroundColor, title: title, titleColor: titleColor, radius: radius, fontSize: fontSize)
+    }
+}
+
+
+// MARK: - Methods
+extension SHButton {
+    
+    func set(image: UIImage,withTint color: UIColor) {
+        self.setImage(image, for: .normal)
+        self.imageView?.image = self.image(for: .normal)?.withRenderingMode(.alwaysTemplate)
+        self.tintColor = color
+    }
+    
+    
+    fileprivate func setup(backgroundColor: UIColor, title: String, titleColor: UIColor, radius: CGFloat, fontSize: CGFloat) {
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.layer.cornerRadius = radius
+        self.titleLabel?.font = UIFont.systemFont(ofSize: fontSize, weight: .heavy)
+        self.layer.masksToBounds = false
+        self.clipsToBounds = true
+        self.imageView?.contentMode = .scaleAspectFill
+    }
+}
