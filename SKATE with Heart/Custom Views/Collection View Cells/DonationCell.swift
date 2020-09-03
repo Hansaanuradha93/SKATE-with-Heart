901105274876c756df246757eb5721a05e0d63f8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 22:23:41 +0530

    basic collection view configured in Donation List

---
+++ SKATE with Heart/Custom Views/Collection View Cells/DonationCell.swift
@@ -0,0 +1,13 @@
+//
+//  DonationCell.swift
+//  SKATE with Heart
+//
+//  Created by Hansa Anuradha on 9/3/20.
+//  Copyright © 2020 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class DonationCell: UICollectionViewCell {
+    
+}
