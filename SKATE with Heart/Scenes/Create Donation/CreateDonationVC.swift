d43730ccde99fb231aa13667a352278bdd30008f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-03 20:25:20 +0530

    Validation added to Create Donation screen

--- SKATE with Heart/Scenes/Create Donation/CreateDonationVC.swift
+++ SKATE with Heart/Scenes/Create Donation/CreateDonationVC.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class CreateDonationVC: UIViewController {
     
+    fileprivate let createDonationViewModel = CreateDonationVM()
+    
     fileprivate let fullNameTextField = SHTextField(padding: 16, placeholderText: "Your name", radius: 25)
     fileprivate let donationTextField = SHTextField(padding: 16, placeholderText: "What are you donating", radius: 25)
     fileprivate let pickupLocationTextField = SHTextField(padding: 16, placeholderText: "Pickup location", radius: 25)
@@ -18,6 +20,53 @@ class CreateDonationVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        addTargets()
+        setupDonationViewModelObserver()
+    }
+    
+    
+    @objc fileprivate func handleTapDismiss() {
+        view.endEditing(true)
+    }
+    
+    
+    @objc fileprivate func handleTextChange(textField: UITextField) {
+        createDonationViewModel.fullName = fullNameTextField.text
+        createDonationViewModel.donation = donationTextField.text
+        createDonationViewModel.pickupLocation = pickupLocationTextField.text
+    }
+    
+    
+    fileprivate func setupDonationViewModelObserver() {
+        createDonationViewModel.bindalbeIsFormValid.bind { [weak self] isFormValid in
+            guard let self = self, let isFormValid = isFormValid else { return }
+            if isFormValid {
+                self.saveDonationButton.backgroundColor = UIColor.appColor(color: .darkPink)
+                self.saveDonationButton.setTitleColor(.white, for: .normal)
+            } else {
+                self.saveDonationButton.backgroundColor = UIColor.appColor(color: .lightGray)
+                self.saveDonationButton.setTitleColor(.gray, for: .disabled)
+            }
+            self.saveDonationButton.isEnabled = isFormValid
+        }
+        
+//        signupViewModel.bindableIsRegistering.bind { [weak self] isRegistering in
+//            guard let self = self, let isRegistering = isRegistering else { return }
+//            if isRegistering {
+//                self.showPreloader()
+//            } else {
+//                self.hidePreloader()
+//            }
+//        }
+    }
+    
+    
+    fileprivate func addTargets() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+        fullNameTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        donationTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        pickupLocationTextField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+//        signupButton.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
     }
     
     
---
+++ SKATE with Heart/Scenes/Create Donation/CreateDonationVM.swift
@@ -0,0 +1,61 @@
+import UIKit
+import Firebase
+
+class CreateDonationVM {
+    
+    // MARK: Properties
+    var fullName: String? { didSet { checkFormValidity() } }
+    var donation: String? { didSet { checkFormValidity() } }
+    var pickupLocation: String? { didSet { checkFormValidity() } }
+    
+    
+    // MARK: Bindlable
+    var bindalbeIsFormValid = Bindable<Bool>()
+    var bindableIsRegistering = Bindable<Bool>()
+}
+
+
+// MARK: - Methods
+extension CreateDonationVM {
+    
+//    func performSignUp(completion: @escaping (Error?) -> ()) {
+//        guard let email = email, let password = password else { return }
+//        self.bindableIsRegistering.value = true
+//        Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+//            guard let self = self else { return }
+//            if let error = error {
+//                self.bindableIsRegistering.value = false
+//                completion(error)
+//                return
+//            }
+//            self.saveInfoToFirestore(completion: completion)
+//        }
+//    }
+    
+    
+//    fileprivate func saveInfoToFirestore(completion: @escaping (Error?) -> ()) {
+//        let uid = Auth.auth().currentUser?.uid ?? ""
+//        let userInfo = [
+//            "uid": uid,
+//            "fullname": fullName ?? "",
+//            "email": email ?? "",
+//            "isAdminUser": false
+//            ] as [String : Any]
+//        Firestore.firestore().collection("users").document(uid).setData(userInfo) { [weak self] error in
+//            guard let self = self else { return }
+//            self.bindableIsRegistering.value = false
+//            if let error = error {
+//                completion(error)
+//                return
+//            }
+//            print("Authentication successfull")
+//            completion(nil)
+//        }
+//    }
+    
+    
+    func checkFormValidity() {
+        let isFormValid = fullName?.isEmpty == false && donation?.isEmpty == false && pickupLocation?.isEmpty == false
+        bindalbeIsFormValid.value = isFormValid
+    }
+}
