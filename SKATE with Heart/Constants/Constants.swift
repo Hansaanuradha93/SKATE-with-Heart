60d10fba366433e834b0aea61c1847c88f722612
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-26 22:13:14 +0530

    String constants added

---
+++ SKATE with Heart/Constants/Constants.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct Strings {
+    
+    // Titles
+    static let donationList = "Donation List"
+    
+    // Alert
+    static let areYouSure = "Are you sure"
+    static let didYouPickupTheDonation = "Did you pickup the donation?"
+    static let yes = "Yes"
+    static let no = "No"
+    
+    
+    // View Model
+    static let donationPickedUpSuceessfully = "Donation Picked Up Successfully"
+}
