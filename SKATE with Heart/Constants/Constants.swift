43dadcea6b7a5f1d09b9867f921c0f51dc9a694b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-26 22:26:23 +0530

    Created Donation strings refactored

--- SKATE with Heart/Constants/Constants.swift
+++ SKATE with Heart/Constants/Constants.swift
@@ -4,14 +4,27 @@ struct Strings {
     
     // Titles
     static let donationList = "Donation List"
+    static let createDonation = "Create Donation"
     
-    // Alert
+    // Alerts
+    static let successful = "Successful"
+    static let failed = "Failed"
+    static let somethingWentWrong = "Something Went Wrong"
     static let areYouSure = "Are you sure"
     static let didYouPickupTheDonation = "Did you pickup the donation?"
+    static let donationSavedSuccessful = "Donation saved successfully"
     static let yes = "Yes"
     static let no = "No"
+    static let ok = "OK"
     
+    // Placeholders
+    static let yourName = "Your name"
+    static let whatAreYouDonating = "What are you donating"
+    static let pickupLocation = "Pickup location"
     
-    // View Model
+    // View Models
     static let donationPickedUpSuceessfully = "Donation Picked Up Successfully"
+    
+    // Buttons
+    static let saveDonation = "Save Donation"
 }
