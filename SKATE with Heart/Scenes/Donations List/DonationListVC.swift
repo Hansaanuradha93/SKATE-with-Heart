eda4e8c6539da82436332566ddb3c51610245a6e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-28 22:05:30 +0530

    Donation List VC code cleaned

--- SKATE with Heart/Scenes/Donations List/DonationListVC.swift
+++ SKATE with Heart/Scenes/Donations List/DonationListVC.swift
@@ -17,55 +17,6 @@ class DonationListVC: UIViewController {
 }
 
 
-// MARK: - Methods
-extension DonationListVC {
-    
-    fileprivate func fetchDonations() {
-        showPreloader()
-        viewModel.fetchData { [weak self] status in
-            guard let self = self else { return }
-            self.hidePreloader()
-            if status { DispatchQueue.main.async { self.collectionview.reloadData() } }
-        }
-    }
-    
-    
-    fileprivate func setupUI() {
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = Strings.donationList
-        view.backgroundColor = .white
-        
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        layout.minimumLineSpacing = 40
-        layout.itemSize = CGSize(width: 275, height: 450)
-        
-        collectionview = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionview.dataSource = self
-        collectionview.register(DonationCell.self, forCellWithReuseIdentifier: DonationCell.reuseID)
-        collectionview.showsHorizontalScrollIndicator = false
-        collectionview.backgroundColor = .white
-        view.addSubview(collectionview)
-        collectionview.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
-    }
-    
-    
-    fileprivate func displayAlert(donation: Donation?) {
-        self.presentAlertAction(title: Strings.areYouSure, message: Strings.didYouPickupTheDonation, rightButtonTitle: Strings.yes, leftButtonTitle: Strings.no, rightButtonAction:  { (_) in
-            self.updatePickupState(donation: donation)
-        })
-    }
-    
-    
-    fileprivate func updatePickupState(donation: Donation?) {
-        viewModel.pickupDonation(donation: donation) { status, message in
-            if status { print(message) }
-        }
-    }
-}
-
-
 // MARK: - UICollectionViewDataSource
 extension DonationListVC: UICollectionViewDataSource {
     
@@ -88,5 +39,54 @@ extension DonationListVC: DonationCellDelegate {
     
     func pickUpButtonTapped(donation: Donation?) {
         displayAlert(donation: donation)
+    }
+}
+
+
+// MARK: - Fileprivate Methods
+fileprivate extension DonationListVC {
+    
+    func fetchDonations() {
+        showPreloader()
+        viewModel.fetchData { [weak self] status in
+            guard let self = self else { return }
+            self.hidePreloader()
+            if status { DispatchQueue.main.async { self.collectionview.reloadData() } }
+        }
+    }
+    
+    
+    func setupUI() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = Strings.donationList
+        view.backgroundColor = .white
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        layout.minimumLineSpacing = 40
+        layout.itemSize = CGSize(width: 275, height: 450)
+        
+        collectionview = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionview.dataSource = self
+        collectionview.register(DonationCell.self, forCellWithReuseIdentifier: DonationCell.reuseID)
+        collectionview.showsHorizontalScrollIndicator = false
+        collectionview.backgroundColor = .white
+        view.addSubview(collectionview)
+        collectionview.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+    }
+    
+    
+    func displayAlert(donation: Donation?) {
+        self.presentAlertAction(title: Strings.areYouSure, message: Strings.didYouPickupTheDonation, rightButtonTitle: Strings.yes, leftButtonTitle: Strings.no, rightButtonAction:  { (_) in
+            self.updatePickupState(donation: donation)
+        })
+    }
+    
+    
+    func updatePickupState(donation: Donation?) {
+        viewModel.pickupDonation(donation: donation) { status, message in
+            if status { print(message) }
+        }
     }
 }
